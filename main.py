7cddc8631f1bd7b8b6f923528db3da8a3c8af155
OmuUser <forces.ca@bk.ru>
2022-04-15 11:09:56 +0300

    Add file, new test bot

--- main.py
+++ main.py
@@ -2,17 +2,40 @@
 import discord
 from discord.ext import commands
 from config import settings
+import asyncio
+import json
+import requests
 
-# Токен бота OTU5NDI0Njc1ODk5ODk5OTc0.Ykbr2w.EWLnHO71h9vnGSQiKuezxHoTgQ4
 
+bot = commands.Bot(command_prefix=settings['prefix']) # Так как мы указали префикс в settings, обращаемся к словарю с ключом prefix.
 
-bot = commands.Bot(command_prefix = settings['prefix']) # Так как мы указали префикс в settings, обращаемся к словарю с ключом prefix.
+@bot.event # Эта функция позволяет отправлять сообщение в чат дискорда через консоль
+async def on_ready():
+    for i in range(1):
+        try:
+            channel = await bot.fetch_channel(input('id Канала: '))
+            await channel.send(content=input('Ваше сообщение: '))
+        except Exception:
+            print('Не хватает прав')
 
-@bot.command() # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
+# @bot.event
+# async def on_ready():
+#     print('Bot is ready')
+
+@bot.command()
 async def hello(ctx): # Создаём функцию и передаём аргумент ctx.
     author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
 
-    await ctx.send(f'Hello, {author.mention}!') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+    await ctx.send(f'Привет!, {author.mention}!, я готов к дальнейшим экзекуциям...') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+
+@bot.command()
+async def pikachu(ctx):
+    response = requests.get('https://some-random-api.ml/img/pikachu') # Get-запрос
+    json_data = json.loads(response.text) # Извлекаем JSON
+
+    embed = discord.Embed(color=0xff9900, title='Random Pikachu') # Создание Embed'a
+    embed.set_image(url=json_data['link']) # Устанавливаем картинку Embed'a
+    await ctx.send(embed=embed) # Отправляем Embed
 
 bot.run(settings['token']) # Обращаемся к словарю settings с ключом token, для получения токена
 
